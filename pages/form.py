e1da821987a9f7d4e0dc6e790302ab491c9d7ae1
chaymae255 <78619819+chaymae255@users.noreply.github.com>
2025-04-01 14:21:59 +0000

    test

--- pages/form.py
+++ pages/form.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.title("FormPage")
+# Form for gathering user input
+with st.form("user_form"):
+    # Form fields
+    name = st.text_input("Your Name")
+    age = st.number_input("Your Age", min_value=1, max_value=100)
+    gender = st.radio("Select Gender", ["Male", "Female", "Other"])
+    feedback = st.text_area("Your Feedback")
+    
+    # Form submission button
+    submit_button = st.form_submit_button("Submit")
+    
+# Display the results after submission
+if submit_button:
+    st.write(f"Name: {name}")
+    st.write(f"Age: {age}")
+    st.write(f"Gender: {gender}")
+    st.write(f"Feedback: {feedback}")
 
 if st.button("Go Back to Home"):
-    st.switch_page("streamlit_app")
+    st.switch_page("streamlit_app.py")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("technovation")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 if st.button("Go to Form Page"):
-    st.switch_page("form")
+    st.switch_page("./pages/form.py")
